27ce2b1742f180bb94bec9b68a2f33b4f195debb
Melissa Baker <melissa@Melissas-MacBook-Pro.local>
2020-02-15 07:33:36 -0700

    Added topic and command file generation/exploration scripts to development/

--- development/parse-commands-json.js
+++ development/parse-commands-json.js
@@ -1,31 +1,89 @@
 const fs = require("fs");
+var colors = require("colors");
 
 const rawdata = fs.readFileSync("commands.json");
 const commands = JSON.parse(rawdata);
 
-let groups = {};
-let props = {};
-let options = {};
-
 for (command of Object.keys(commands)) {
-  const group = commands[command].group;
-  const obj_props = Object.keys(commands[command]);
-  groups[group] = (groups[group] || 0) + 1;
-  for (prop of obj_props) {
-    props[prop] = (props[prop] || 0) + 1;
-    if (prop === "arguments") {
-      const args = commands[command][prop].map(arg =>
-        arg.command ? arg.command : arg.name
-      );
-      console.log(command, args);
-    }
+  const argsObj = commands[command].arguments;
+  let argsString = command;
+  console.log(colors.magenta(argsString));
+  if (argsObj) {
+    argsObj.forEach(arg => {
+      argsString += " " + getArgs(arg);
+    });
   }
+  console.log(argsString);
+  console.log("");
 }
 
-console.log(groups);
-console.log(props);
+function getArgs(a) {
+  let s = "";
 
-// fs.writeFile("../docs/test.md", "# Hello content!", function(err) {
-//   if (err) throw err;
-//   console.log("Saved!");
-// });
+  if (a.command) {
+    s += " " + a.command;
+  }
+
+  if (a.enumargs) {
+    let eargs = "";
+    for (earg of a.enumargs) {
+      let v = earg.name ? earg.name + " " : "";
+      v += earg.arguments ? getArgs(earg.arguments) : "";
+
+      v = v.trim();
+      if (v.includes(" ")) {
+        v = "(" + v + ")";
+      }
+      eargs += v + "|";
+    }
+
+    if (eargs.length > 0) {
+      eargs = eargs.slice(0, eargs.length - 1);
+    }
+    s += " " + eargs;
+  } else if (a.enum) {
+    s += " " + a.enum.join("|");
+  } else {
+    let names = getNames(a);
+    let subs = "";
+
+    for (name of names) {
+      subs += " " + name;
+    }
+    subs = subs.trim();
+    s += " " + subs;
+
+    if (a.variadic) {
+      if (names.length === 0) {
+        s += " [" + subs + " ...]";
+      } else {
+        s += " [" + names[names.length - 1] + "...]";
+      }
+    }
+    if (a.multiple) {
+      s += " ...";
+    }
+  }
+  s = s.trim();
+  if (a.optional) {
+    s = "[" + s + "]";
+  }
+
+  return s;
+}
+
+function getNames(argsObj) {
+  let names = [];
+
+  const name = argsObj.name;
+
+  if (name) {
+    names = typeof name === "string" ? [...names, name] : [...names, ...name];
+  } else if (!argsObj.command) {
+    argsObj.forEach(item => {
+      names.push(item.name);
+    });
+  }
+
+  return names;
+}
