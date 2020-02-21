72fff354492bd362aef05c9cec631b85cc2ae8ee
Melissa Baker <melissa@PseudoProfoundBaloneyPants.local>
2020-02-21 12:17:21 -0700

    Modified siteConfig.js for publishing site to github.io

--- website/siteConfig.js
+++ website/siteConfig.js
@@ -23,15 +23,15 @@ const users = [
 const siteConfig = {
   title: "Tile38-test", // Title for your website.
   tagline: "Ultra Fast Geospatial Database & Geofencing Server",
-  url: "https://your-docusaurus-test-site.com", // Your website URL
-  baseUrl: "/", // Base URL for your project */
+  url: "https://melbania.github.io", // Your website URL
+  baseUrl: "/tile38-test/", // Base URL for your project */
   // For github.io type URLs, you would set the url and baseUrl like:
   //   url: 'https://facebook.github.io',
   //   baseUrl: '/test-site/',
 
   // Used for publishing and more
-  projectName: "Tile38",
-  organizationName: "Tile38",
+  projectName: "tile38-test",
+  organizationName: "melbania",
   // For top-level user or org sites, the organization is still the same.
   // e.g., for the https://JoelMarcey.github.io site, it would be set like...
   //   organizationName: 'JoelMarcey'
