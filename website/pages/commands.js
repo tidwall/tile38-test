d85f4301c5dcfdfe04e593ded71a712c4273b6f7
Melissa Baker <missymae76@gmail.com>
2020-02-26 10:08:44 -0700

    Modified css styling for homeContainer element to remove extra padding on small screens

---
+++ website/pages/commands.js
@@ -0,0 +1,10 @@
+const React = require("react");
+
+class Commands extends React.Component {
+  state = {};
+  render() {
+    return <h1>I am the Commands page.</h1>;
+  }
+}
+
+module.exports = Commands;
--- website/siteConfig.js
+++ website/siteConfig.js
@@ -38,6 +38,7 @@ const siteConfig = {
 
   // For no header links in the top nav bar -> headerLinks: [],
   headerLinks: [
+    { page: "commands", label: "Commands" },
     { doc: "topics/installation", label: "Docs" },
     { page: "license", label: "License" },
     { blog: true, label: "Blog" },
